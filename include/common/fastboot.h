5046767760754736e0a307171b2b4cc4e527e0b1
Dan Murphy <dmurphy@ti.com>
2012-03-09 14:14:16 -0600

    fastboot: add license to the header file
    Add appropriate licensing to the fastboot header.
    
    Change-Id: Id5c184762789c6b22e0fbc46a5724257916581ba
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- include/common/fastboot.h
+++ include/common/fastboot.h
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2010, The Android Open Source Project.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Neither the name of The Android Open Source Project nor the names
+ *    of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
 #ifndef _FASTBOOT_H_
 #define _FASTBOOT_H_
 
@@ -27,7 +55,6 @@ struct fastboot_ptentry {
 
 void do_fastboot(void);
 char *get_serial_number(void);
-
 void fastboot_flash_reset_ptn(void);
 void fastboot_flash_add_ptn(fastboot_ptentry *ptn, int count);
 void fastboot_flash_dump_ptn(int count);
@@ -41,7 +68,6 @@ extern char *get_ptn_size(char *buf, const char *ptn);
 
 void do_fastboot(void) { return; };
 char *get_serial_number(void) { return 0; };
-
 void fastboot_flash_reset_ptn(void) { return; };
 void fastboot_flash_add_ptn(fastboot_ptentry *ptn, int count) { return; };
 void fastboot_flash_dump_ptn(int count) { return; };
