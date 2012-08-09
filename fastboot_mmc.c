bfea633d794814156761e57c0fc30a6083e5c7c8
Regis Goupil <x176713@ti.com>
2012-08-09 12:26:54 -0500

    fastboot:mmc:move struct definitions & #defines to header file
    Moving:
    struct partition_type
    const u8 random_uuid
    const u8 partition_type
    struct efi_header
    struct efi_entry
    struct ptable
    definitons from fastboot_mmc.c to fasboot.h
    
    Change-Id: I59d5b9b356a3cd2ba86bdf401d28242dc2238282
    Signed-off-by: Regis Goupil <x0176713@ti.com>
    Signed-off-by: Christina Warren <cawarren@ti.com>

--- fastboot_mmc.c
+++ fastboot_mmc.c
@@ -47,61 +47,6 @@
 #if defined CONFIG_FASTBOOT
 #include <common/fastboot.h>
 
-#define EFI_VERSION 0x00010000
-#define EFI_ENTRIES 128
-#define EFI_NAMELEN 36
-
-static const u8 partition_type[16] = {
-	0xa2, 0xa0, 0xd0, 0xeb, 0xe5, 0xb9, 0x33, 0x44,
-	0x87, 0xc0, 0x68, 0xb6, 0xb7, 0x26, 0x99, 0xc7,
-};
-
-static const u8 random_uuid[16] = {
-	0xff, 0x1f, 0xf2, 0xf9, 0xd4, 0xa8, 0x0e, 0x5f,
-	0x97, 0x46, 0x59, 0x48, 0x69, 0xae, 0xc3, 0x4e,
-};
-
-struct efi_entry {
-	u8 type_uuid[16];
-	u8 uniq_uuid[16];
-	u64 first_lba;
-	u64 last_lba;
-	u64 attr;
-	u16 name[EFI_NAMELEN];
-};
-
-struct efi_header {
-	u8 magic[8];
-
-	u32 version;
-	u32 header_sz;
-
-	u32 crc32;
-	u32 reserved;
-
-	u64 header_lba;
-	u64 backup_lba;
-	u64 first_lba;
-	u64 last_lba;
-
-	u8 volume_uuid[16];
-
-	u64 entries_lba;
-
-	u32 entries_count;
-	u32 entries_size;
-	u32 entries_crc32;
-} __attribute__((packed));
-
-struct ptable {
-	u8 mbr[512];
-	union {
-		struct efi_header header;
-		u8 block[512];
-	};
-	struct efi_entry entry[EFI_ENTRIES];
-};
-
 static struct partition *partitions;
 static struct ptable the_ptable;
 
