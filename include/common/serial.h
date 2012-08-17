a8e38d6905afcdfde341f9c6a8d900c7f0b8c78d
Regis Goupil <x0176713@ti.com>
2012-08-17 13:57:28 -0500

    serial: defines: moving to serial.h
    Create include/common/serial.h to host the data
    definitions contained in arch/common/serial.c
    
    Change-Id: I082d0923f197c9d08855101bcf53ef5f3f3100e7
    Signed-off-by: Regis Goupil <x0176713@ti.com>
    Signed-off-by: Christina Warren <cawarren@ti.com>

--- include/common/serial.h
+++ include/common/serial.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
- * All rights reserved.
+ * Copyright (C) 2012, Texas Instruments, Inc.
+ * Texas Instruments, <www.ti.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -26,43 +26,25 @@
  * SUCH DAMAGE.
  */
 
-#include <aboot/aboot.h>
-#include <aboot/io.h>
+#if defined CONFIG_IS_OMAP4
+#include <omap4/hw.h>
+#elif defined CONFIG_IS_OMAP5
+#include <omap5/hw.h>
+#endif
 
-#include <common/serial.h>
+#define OFF_RBR		0x00
+#define OFF_THR		0x00
+#define OFF_DLL		0x00
+#define OFF_IER		0x04
+#define OFF_DLM		0x04
+#define OFF_FCR		0x08
+#define OFF_IIR		0x08
+#define OFF_LCR		0x0C
+#define OFF_MCR		0x10
+#define OFF_LSR		0x14
+#define OFF_MSR		0x18
+#define OFF_SCR		0x1C
+#define OFF_MDR1	0x20
 
-void serial_init(void)
-{
-	unsigned divisor = CONFIG_SERIAL_CLK_HZ / 16 / CONFIG_BAUDRATE;
-
-	WR(0x00, IER);
-	WR(0x07, MDR1); /* reset */
-	WR(0x83, LCR);  /* 8N1 + banksel */
-	WR(divisor & 0xFF, DLL);
-	WR(divisor >> 8, DLM);
-	WR(0x03, LCR);  /* 8N1 */
-	WR(0x03, MCR);  /* DTR, RTS */
-	WR(0x07, FCR);  /* reset and enable FIFO */
-	WR(0x00, MDR1); /* run */
-}
-
-static inline void _serial_putc(char c)
-{
-	while (!(RD(LSR) & 0x20))
-		;
-	WR(c, THR);
-}
-
-void serial_putc(char c)
-{
-	if (c == '\n')
-		_serial_putc('\r');
-	_serial_putc(c);
-}
-
-void serial_puts(const char *s)
-{
-	while (*s)
-		serial_putc(*s++);
-}
-
+#define WR(val, addr) writeb(val, CONFIG_SERIAL_BASE + OFF_##addr)
+#define RD(addr) readb(CONFIG_SERIAL_BASE + OFF_##addr)
